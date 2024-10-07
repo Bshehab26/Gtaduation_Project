c13fd0a17828bfe71a29fa99dc86f5a668451ed1
AG2k2 <ammarnagy369@gmail.com>
2024-10-07 21:17:11 +0300

    1

--- resources/views/layouts/dashboard/includes/sidebar.blade.php
+++ resources/views/layouts/dashboard/includes/sidebar.blade.php
@@ -39,6 +39,19 @@
             </ul>
         </li><!-- End Components Nav -->
 
+        <li class="nav-item">
+            <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-menu-button-wide"></i><span>Events</span><i class="bi bi-chevron-down ms-auto"></i>
+            </a>
+            <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                <li>
+                    <a href="components-alerts.html">
+                    <i class="bi bi-circle"></i><span>All events ({{ \App\Models\Event::count() }})</span>
+                    </a>
+                </li>
+            </ul>
+        </li><!-- End Events Nav -->
+
         {{-- Users --}}
         <li class="nav-item">
             <a class="nav-link collapsed" data-bs-target="#users-nav" data-bs-toggle="collapse" href="#">
